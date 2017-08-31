eda4268633ae866ebaaafa54db5ad90762b1d885
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-31 11:30:48 -0500

    can add and delete a user

--- app.js
+++ app.js
@@ -12,11 +12,84 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-
+// renders an index page
 app.get('/', function(req, res) {
   res.render('index');
 
 });
+
+// hrefs are gets
+// forms are posts
+// uses a link to get all of the users on a different page
+// FINDS all users
+app.get('/users', function(req, res) {
+  models.User.findAll()
+  .then(function(users){
+    res.render('users', {users: users});
+  })
+})
+
+// form to add a user
+app.get('/add', function(req, res) {
+    res.render('add');
+  });
+
+// create new here
+app.post('/newuser', function (req, res){
+
+  const newUser = models.User.build({
+  name: req.body.name,
+  email: req.body.email,
+  bio: req.body.bio
+});
+
+newUser.save().then(function () {
+  res.redirect('users');
+  })
+});
+
+
+// steps for delete:
+// create delete button on mustache page, form needs to be post
+// need to pass ID of user when we post, so id of button is unique
+// create post route that deletes user and shows list of users
+// can name user button anything, gets passed from the {{id}} in the form, it is a number
+// app.post('/delete/:idOfUserButton', function (req, res){
+//   models.User.findById(req.params.idOfUserButton).then(function (users) {
+//              User.destroy().then(function () {
+//                  res.redirect("/users");
+//              })
+//      })
+//
+// });
+
+
+app.post('/delete/:idOfUserButton', function (req, res){
+  models.User.destroy({where: {id: req.params.idOfUserButton}}).then(function (users) {
+            
+                 res.redirect("/users");
+             })
+     })
+
+});
+
+
+// newUser.save()
+// .then(function (newUser) {
+//   return models.User.findAll()
+// })
+// .then(function(users){
+//   res.render('users', {users: users});
+//
+//   })
+
+// user data is the returned array, namming convention matches the paramater in the promise function
+// app.get('matches route put in href or forms', function(req, res) {
+//   models.User.find()
+//   .then(function(user data){
+//     res.render('mustache file', {mustache key name: user data});
+//   })
+// })
 
 
 app.listen(3000, function() {
